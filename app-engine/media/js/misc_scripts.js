6076dbcad2c789851704c171c14928a61673ba0e
Jose Garrido <j.garrido@epubli.de>
2012-03-28 18:12:49 +0200

    new branch (dev) and file into that branch

--- app-engine/media/js/misc_scripts.js
+++ app-engine/media/js/misc_scripts.js
@@ -1,9 +1,40 @@
-$(document).ready(function(){
+var ytp; // Youtube Flash Player Object
 
-    $("div#video iframe").attr("width",viewport().width);
-    $("div#video iframe").attr("height",viewport().height);
+$(document).ready(function(){
+    
+    console.log("Viewport Dimentions adjusted");
+    v = viewport();
+    $("div#video object").attr("width",v.width);
+    $("div#video object").attr("height",v.height);
+
+    $("img#pause").click(function() {
+
+	state = ytp.getPlayerState();
+
+	if (state == 1){ // Playing
+	    ytp.pauseVideo();
+	    console.log("Pause");
+	}
+	else if (state == 2){ // Paused
+	    ytp.playVideo();
+	    console.log("Play");
+	}
+
+    });
 
 });
+
+function onYouTubePlayerReady(playerid) {
+    console.log("Youtube Player Registered")
+    ytp = document.getElementById("ytplayer");
+
+    // Initial video
+
+    ytp.loadVideoById('flmB63_fpm4',1,'medium');
+
+    console.log(ytp);
+
+}
 
 function viewport() {
     var e = window , a = 'inner';
